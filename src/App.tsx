aceeb601ab53dfa4bde23e3d3977139828767d2d
mengfanfei <mengff0924@163.com>
2022-02-08 16:30:50 +0800

    修改样式

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
 import {Outlet, useLocation} from 'react-router-dom'
 import MyHeader from '@/components/MyHeader'
-import { useEffect } from 'react'
-import { wxConfig } from '@/utils/wxUtils'
+// import { useEffect } from 'react'
+// import { wxConfig } from '@/utils/wxUtils'
 
 function App() {
   const location = useLocation()
 
   // 获取微信配置信息，模拟componentDidMount, 只运行一次
-  useEffect(() => {
-    wxConfig()
-  }, [])
+  // useEffect(() => {
+  //   wxConfig()
+  // }, [])
 
   if (location.pathname === '/login') {
     return (
--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -2,7 +2,7 @@ import {Button} from 'antd-mobile'
 
 const Login = () => {
   return (
-    <div className='w-12'>
+    <div className='w-40'>
       login
       <Button color={'danger'} size={'mini'}>按钮Login</Button>
     </div>
